5935c31dfa1e12efc2ce6faa0ed29a57929a972e
ChitteshK139 <128819733+ChitteshK139@users.noreply.github.com>
2024-03-05 18:50:45 +0530

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello from PES1UG21CS160\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully executed pipeline\n";
+  return 0;
+}
